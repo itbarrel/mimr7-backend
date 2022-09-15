0014af51a935639c24ee88831cb49300f6b8dba9
Faiz Ali <faizali.virgo@gmail.com>
2022-09-15 19:39:26 +0500

    change collection get api

--- src/services/resources/collection.js
+++ src/services/resources/collection.js
@@ -1,9 +1,30 @@
+const { Op } = require('sequelize')
 const models = require('../../models')
 const ResourceService = require('./resource')
+const storage = require('../../utils/cl-storage')
 
 class CollectionService extends ResourceService {
     constructor() {
         super(models.Collection)
+    }
+
+    async all(query = {}, offset = 1, limit = 20, sort = {}) {
+        const { accountId, role } = storage.get('decoded')
+        if (role !== 'SuperAdmin') {
+            query.AccountId = accountId
+        }
+        const sorted = []
+        Object.keys(sort).map((key) => sorted.push([key, sort[key]]))
+        query.title && query.title !== '' ? query.title = { [Op.iLike]: `%${query.title}%` } : delete query.title
+        const options = {
+            // offset: offset * (limit + 1),
+            where: query,
+            page: offset,
+            paginate: limit,
+            order: sorted,
+
+        }
+        return this.model.paginate(options)
     }
 }
 
