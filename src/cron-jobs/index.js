59f106737eaacc12bae2f992c2ff56cbd87f1297
Faiz Ali <faizali.virgo@gmail.com>
2023-05-09 14:00:23 +0500

    correct messagSchedule job add new job for classList deactivated

--- src/cron-jobs/index.js
+++ src/cron-jobs/index.js
@@ -12,36 +12,66 @@ const hashGenerator = require('../utils/hashGenerator')
 
 cron.schedule('  0 8-22 * * *', async () => {
     jobQueue.push(async () => {
-        const classes = await ClassListScheduleService.findByQuery({ active: true })
+        const classes = await ClassListScheduleService.findByQuery({ active: true }, false)
         if (classes) {
-            const { ContentId, ClassListId } = classes
-            const messages = await MessageService.findByQuery({ ContentId }, false)
-            const classList = await ClassListService.findByQuery({ id: ClassListId })
-            const content = await ContentService.findById(ContentId)
-            const account = await AccountService.findById(content.AccountId)
+            classes.map(async (klass) => {
+                const { ContentId, ClassListId } = klass
+                const messages = await MessageService.findByQuery({ ContentId }, false)
+                const classList = await ClassListService.findByQuery({ id: ClassListId })
+                const content = await ContentService.findById(ContentId)
+                const account = await AccountService.findById(content.AccountId)
 
-            const students = await classList.getStudents()
-            students.map(async (student) => {
-                const randomIndex = Math.floor(Math.random() * messages.length)
-                const randomMsaage = messages[randomIndex]
-                const findMessageSchedule =
-                    await MessageScheduleService.findByQuery({ StudentId: student.id, MessageId: randomMsaage.id }, false)
-                const { name } = randomMsaage
-                const generatedhash = hashGenerator(32)
-                const messageScheduleObj = {
-                    MessageId: randomMsaage.id,
-                    StudentId: student.id,
-                    AccountId: account.id,
-                    hash: generatedhash,
-                    count: 1,
-                }
-                if (!findMessageSchedule || findMessageSchedule && findMessageSchedule.length <= 4) {
-                    const messageSchedule = await MessageScheduleService.create(messageScheduleObj)
-                    if (messageSchedule) {
-                        await student.messageEmail(content.title, messageSchedule.hash)
+                const students = await classList.getStudents()
+                students.map(async (student) => {
+                    const randomIndex = Math.floor(Math.random() * messages.length)
+                    const randomMsaage = messages[randomIndex]
+                    const findMessageSchedule =
+                        await MessageScheduleService.findByQuery({ StudentId: student.id, MessageId: randomMsaage.id }, false)
+                    const generatedhash = hashGenerator(32)
+                    const messageScheduleObj = {
+                        MessageId: randomMsaage.id,
+                        StudentId: student.id,
+                        AccountId: account.id,
+                        hash: generatedhash,
+                        count: 1,
                     }
+                    if (!findMessageSchedule || findMessageSchedule && findMessageSchedule.length <= 4) {
+                        const messageSchedule = await MessageScheduleService.create(messageScheduleObj)
+                        if (messageSchedule) {
+                            await student.messageEmail(content.title, messageSchedule.hash)
+                        }
+                    }
+                })
+            })
+
+        }
+    })
+    jobQueue.start((err) => {
+        if (err) throw err
+    })
+}, {
+    scheduled: true,
+    timezone: 'Asia/Karachi'
+})
+cron.schedule('  5 24 * * *', async () => {
+    jobQueue.push(async () => {
+        const classes = await ClassListScheduleService.findByQuery({ active: true }, false)
+        if (classes) {
+            classes.map(async (klass) => {
+                const { dataValues } = klass
+                const klassEndingDate = klass.endDate
+                const klassendDate = klassEndingDate.getDate()
+                const KlassendMounth = klassEndingDate.getMonth()
+                const getCurrentDate = new Date()
+                const currentDate = getCurrentDate.getDate()
+                const currentMounth = getCurrentDate.getMonth()
+                if (klassendDate < currentDate && (KlassendMounth === currentMounth || KlassendMounth < currentMounth)) {
+                    dataValues.active = false
+                    await ClassListScheduleService.update(dataValues, { id: dataValues.id })
+
                 }
             })
+
         }
     })
     jobQueue.start((err) => {
